c9b289fe11f858d8d157bc8c67a8146e8144cbe3
Pasha <bl4ckbeer@yandex.ru>
2020-04-03 01:36:35 +0500

    sdfsdfsdf

--- js/main.js
+++ js/main.js
@@ -52,5 +52,12 @@ $(document).ready(function () {
           prevEl: '.swiper-button-prev-news',
         },
     });
-    
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+        
 });
