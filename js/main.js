acbd0e39fe976529ddf22bb812a08635123fef32
Pasha <bl4ckbeer@yandex.ru>
2020-04-05 15:33:19 +0500

    fjfjfjfjfjfj

--- js/main.js
+++ js/main.js
@@ -74,6 +74,192 @@ $(document).ready(function () {
         menu.toggleClass('boxing__active')
     
     });
+    var modal = $('.modal'),
+      modalbtn = $('[data-toggle=modal]'),
+      closebtn = $('.modal__close')
+      
+  
+    modalbtn.on('click', function() {
+      $(modal).toggleClass('modal--visible');
+      
+    });
+    closebtn.on('click', function() {
+      $(modal).toggleClass('modal--visible');
+      
+    });
+    //валидация формы 
+  $('.modal__form').validate({
+    errorPlacement: function (error, element) {
+      if (element.attr("type") == "checkbox") {
+          return element.next('label').append(error);
+      }
+  
+       error.insertAfter($(element));
+    },
+    submitHandler: function(form) {
+      $.ajax({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          console.log('Ajax сработа. Ответ сревера'+response);
+          alert('Форма отправлена мы свяжемся с  вами  в течении дня.');
+          $(modal).toggleClass('modal--visible');
+          $(form)[0].reset();
+          
+        }
+      });
+    },
+    errorClass: "invalid",
+    errorElement: "em",
+    rules: {
+      // simple rule, converted to {required:true}
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15,
+        
+      },
+      userPhone: {
+        required: true,
+        minlength: 11,
+        
+        
+      },
+      userPhone: "required",
+      // compound rule
+      userEmail: {
+        required: true,
+        email: true
+      },
+      policycheckbox: "required",
+    },
+    messages: {
+      userName:{
+        required: "Имя обязательно ",
+        minlength: jQuery.validator.format("Имя не короче {0} букв "),
+        maxlength: jQuery.validator.format("Имя не длинее {0} букв ")
+      } ,
+      userPhone:{
+        required: "Телефон обязательно",
+        minlength: jQuery.validator.format("Номер не короче {0} цифр "),
+      },
+      userEmail: {
+        required: "Обязателно укажите Email",
+        email: "Введите в формате sdfsd@asdasd.com"
+        },
+      policycheckbox: "Согласие обязательно"
+      }
+
+  });
+  $('[type=tel]').mask('+7(000)-000-00-00', {placeholder: "Ваш номер телефона"});
+
+  $('.form__price').validate({
+
+        submitHandler: function(form) {
+            $.ajax({
+                type: "POST",
+                url: "send.php",
+                data: $(form).serialize(),
+                success: function (response) {
+                console.log('Ajax сработа. Ответ сревера'+response);
+                alert('Форма отправлена мы свяжемся с  вами  в течении дня.');
+                $(form)[0].reset();
+                
+                }
+            });
+        },
+        errorClass: "invalid",
+        errorElement: "em",
+        rules: {
+        // simple rule, converted to {required:true}
+        userName: {
+            required: true,
+            minlength: 2,
+            maxlength: 15,
+            
+        },
+        userMasange: "required",
+        UserWebsite: "required",
+        // compound rule
+        userEmail: {
+            required: true,
+            email: true
+        }
+        },
+        messages: {
+        userName:{
+            required: "Имя обязательно ",
+            minlength: jQuery.validator.format("Имя не короче {0} букв "),
+            maxlength: jQuery.validator.format("Имя не длинее {0} букв ")
+        } ,
+        userMasange:{
+            required: "Сообщение обязательно",
+        },
+        userEmail: {
+            required: "Обязателно укажите Email",
+            email: "Введите в формате sdfsd@asdasd.com"
+            },
+        UserWebsite: {
+            required: "Обязателно укажите ваш сайт",
+            },
+        }
+
+  });
+  $('.question__form').validate({
+
+    submitHandler: function(form) {
+        $.ajax({
+            type: "POST",
+            url: "send.php",
+            data: $(form).serialize(),
+            success: function (response) {
+            console.log('Ajax сработа. Ответ сревера'+response);
+            alert('Форма отправлена мы свяжемся с  вами  в течении дня.');
+            $(form)[0].reset();
+            
+            }
+        });
+    },
+    errorClass: "invalid",
+    errorElement: "em",
+    rules: {
+    // simple rule, converted to {required:true}
+    userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15,
+        
+    },
+    userMasange: "required",
+    userPhone: "required",
+    // compound rule
+    userEmail: {
+        required: true,
+        email: true
+    }
+    },
+    messages: {
+    userName:{
+        required: "Имя обязательно ",
+        minlength: jQuery.validator.format("Имя не короче {0} букв "),
+        maxlength: jQuery.validator.format("Имя не длинее {0} букв ")
+    } ,
+    userMasange:{
+        required: "Сообщение обязательно",
+    },
+    userEmail: {
+        required: "Обязателно укажите Email",
+        email: "Введите в формате sdfsd@asdasd.com"
+        },
+    userPhone:{
+        required: "Телефон обязательно",
+        minlength: jQuery.validator.format("Номер не короче {0} цифр "),
+        },
+    }
+
+});
+  
 
         
 });
